b9a617e2e51a3ef8efbfc0493af2c11a9156897c
Caio Couto <coutocaio3@gmail.com>
2023-01-24 14:10:55 -0300

    Adding a 'Clear data' button and its logic

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,13 @@
 const form = document.querySelector('#form');
 const setup = new NLWSetup(form);
-const button = document.querySelector('.header-button');
+const headerButton = document.querySelector('.header-button');
+const clearSection = document.querySelector('#clear');
+const clearButton = document.querySelector('.clear-button');
+
+function setFormAndClearSectionActive() {
+    form.classList.add('active');
+    clearSection.classList.add('active');  
+}
 
 function addClassNameToDayElement(className) {
     const days = document.querySelectorAll('.day');
@@ -15,23 +22,33 @@ function addDay() {
         warning.classList.toggle('active');
         setTimeout(() => { warning.classList.toggle('active'); }, 5000);
     }
-    else { setup.addDay(today); }
-    addClassNameToDayElement('form-section');
+    else { 
+        setup.addDay(today); 
+        addClassNameToDayElement('form-section');
+        setFormAndClearSectionActive();
+    }
 }
 
 function loadData() {
-    const storedData = JSON.parse(localStorage.getItem('NLW@Habit')) || {};
-    setup.setData(storedData);
-    setup.load();
-    addClassNameToDayElement('form-section');    
+    let storedData = JSON.parse(localStorage.getItem('NLW@Habit'));
+    if (storedData) {
+        setup.setData(storedData);
+        setup.load();
+        addClassNameToDayElement('form-section');
+        setFormAndClearSectionActive();
+    }
 }
 
 function saveData() {
     localStorage.setItem('NLW@Habit', JSON.stringify(setup.data));
 }
 
-button.addEventListener('click', addDay);
-
-form.addEventListener('change', saveData);
+function clearData() {
+    localStorage.removeItem('NLW@Habit');
+    location.reload();
+}
 
 onload = loadData;
+headerButton.addEventListener('click', addDay);
+form.addEventListener('change', saveData);
+clearButton.addEventListener('click', clearData)
